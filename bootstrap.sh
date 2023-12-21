4f683890cdaad4734bf01a7e669c8237e21a452d
Sergio Brito <sergiodvz9090@gmail.com>
2023-12-21 19:24:08 -0300

    crypto5
    crypto5

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Docker install
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+#Use docker without sudo
+sudo groupadd docker
+sudo usermod -aG docker vagrant
+newgrp docker
+
+#Hadolin install
+sudo wget -O hadolint https://github.com/hadolint/hadolint/releases/download/v2.12.0/hadolint-Linux-x86_64
+sudo mv hadolint /usr/local/bin/hadolint
+sudo chmod +x /usr/local/bin/hadolint
+
+# Install Java
+sudo apt-get install fontconfig openjdk-11-jre -y
+
+#Jenkins install
